01edaed4daa6c9c243d7ab0e6ece9ea59940d4c6
Peter Jenkins <peter.jenkins@csc.fi>
2015-09-17 12:23:22 +0300

    Allow config of OS_USERS_DOMAIN_NAME

--- osswitch.bash
+++ osswitch.bash
@@ -10,6 +10,7 @@ ose() {
   NEW_TENANT_ID=$4
   NEW_USERNAME=$5
   NEW_REGION_NAME=$6
+  NEW_USERS_DOMAIN_NAME=$7
 
   # Check if we already have a password stored
   if [ "$NEW_SERVICE" != "$OS_SERVICE" ]; then
@@ -30,6 +31,7 @@ ose() {
   export OS_TENANT_NAME=$NEW_TENANT_ID # Same as above!
   export OS_USERNAME=$NEW_USERNAME
   export OS_REGION_NAME=$NEW_REGION_NAME
+  export OS_USERS_DOMAIN_NAME=$NEW_USERS_DOMAIN_NAME
 
   PS1="$OS_SERVICE:$OS_ENV \[\033[01;32m\]\u@\h\[\033[00m\] \[\033[01;34m\]\W\[\033[00m\]\[\033[1;32m\]\$\[\033[m\] "
 }
